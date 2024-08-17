6c1e51e84ffb8c1d8fe0f4e0b0f64708485f553c
Mia Coupland <miacoupland@btinternet.com>
2024-08-17 10:12:37 +0100

    provide http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,9 @@ import { WorkshopComponent } from './presentation/workshop/workshop.component';
 import { AboutComponent } from './presentation/about/about.component';
 import { GalleryComponent } from './presentation/gallery/gallery.component';
 import { FooterComponent } from './presentation/footer/footer.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { GalleryTileComponent } from './presentation/gallery/gallery-tile/gallery-tile.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -26,10 +26,11 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule,
-    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    provideHttpClient(),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

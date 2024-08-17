db3c128e78a3a83e60e5194ce1c96eb01f7fcc46
Mia Coupland <miacoupland@btinternet.com>
2024-08-17 11:56:46 +0100

    standalone nav component using material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
+import { } from '@angular/material'
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './presentation/header/header.component';
-import { NavComponent } from './presentation/nav/nav.component';
 import { WorkshopComponent } from './presentation/workshop/workshop.component';
 import { AboutComponent } from './presentation/about/about.component';
 import { GalleryComponent } from './presentation/gallery/gallery.component';
@@ -11,12 +10,12 @@ import { FooterComponent } from './presentation/footer/footer.component';
 import { GalleryTileComponent } from './presentation/gallery/gallery-tile/gallery-tile.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './presentation/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    NavComponent,
     WorkshopComponent,
     AboutComponent,
     GalleryComponent,
@@ -24,8 +23,10 @@ import { provideHttpClient } from '@angular/common/http';
     FooterComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    CommonModule,
+    HeaderComponent,
   ],
   providers: [
     provideAnimationsAsync(),
--- src/app/presentation/nav/nav.component.ts
+++ src/app/presentation/nav/nav.component.ts
@@ -1,25 +1,19 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule, RouterModule],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
   public title: string = 'MC Ceramics';
 
   constructor() {}
-
-  ngOnInit(): void {}
-
-  toggleHamburger(): void {
-    const hamburger = document.getElementById('links');
-    if (hamburger?.classList.contains('hidden')) {
-      hamburger.classList.remove('hidden');
-      hamburger.classList.add('shown');
-    } else {
-      hamburger?.classList.add('hidden');
-      hamburger?.classList.remove('shown');
-    }
-  }
 }
